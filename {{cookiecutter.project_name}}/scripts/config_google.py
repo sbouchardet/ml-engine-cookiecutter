a01801b1affc7ead44b7ca156b108f4e2a1f8a07
André Duarte <andre.duarte@corp.globo.com>
2019-06-18 11:13:36 -0300

    fix prefix typo, version import order, and artefact name repetition bug

--- {{cookiecutter.project_name}}/scripts/config_google.py
+++ {{cookiecutter.project_name}}/scripts/config_google.py
@@ -4,21 +4,16 @@ gs_bucket="{{cookiecutter.gs_bucket}}"
 gs_project = "{{cookiecutter.gs_project}}"
 gs_region = "{{cookiecutter.gs_region}}"
 
-artefact_name = "{{cookiecutter.algorithm_name}}-{}.tar.gz".format(version)
-local_artefact = os.path.join("{{cookiecutter.algorithm_name}}","dist",artefact_name)
-
 with open("{{cookiecutter.algorithm_name}}/version") as v:
     version = v.read().strip()
 
-def storage_artefact(deploy_preffix=None):
+artefact_name = "{{cookiecutter.algorithm_name}}-{}.tar.gz".format(version)
+local_artefact = os.path.join("{{cookiecutter.algorithm_name}}", "dist", artefact_name)
+
+def storage_artefact_path(deploy_prefix=None):
     version_folder = version if deploy_prefix is None else "{}_{}".format(deploy_prefix, version)
     return os.path.join(
         "artefacts",
         "algorithms",
         "{{cookiecutter.algorithm_name}}",
-        version_folder,
-        artefact_name)
-
-
-
-
+        version_folder)
